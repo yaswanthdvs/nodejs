16934d5f5457af11c20b00272b127dac0e33ce6b
Yaswanth DVS <yaswanth.dvs@gmail.com>
2019-07-03 05:57:06 +0000

    Landing page - post route to submit email

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ var router = express.Router();
 /* GET home page. */
 let landing = require('../controllers/landing');
 router.get('/', landing.get_landing);
+router.post('/', landing.submit_lead);
 
 module.exports = router;
